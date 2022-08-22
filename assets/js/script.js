a57b5c991ca79d452221163aeff39e2e5c7850d4
catherinecunningham <cfunk512@gmail.com>
2022-08-21 21:06:32 -0700

    adjust API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,19 @@ var windInput = document.getElementById("wind-input")
 var humidityInput = document.getElementById("humidity-input")
 var UVInput = document.getElementById("UV-input")
 var currentCity = document.getElementById("current-city")
+var searchBtn = document.getElementById("search-Btn")
 
 // Weather API
-function getWeather(e) {
+searchBtn.addEventListener('click', function(e) {
     e.preventDefault()
-
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude "&exclude=" + exclusions + "&appid=581b9ce182221b37e54f248e484a108c")
+    var latitude = "38.5816"
+    var longitude = "121.4944"
+    var exclusions = "minutely, hourly, daily, alerts"
+    
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=" + exclusions + "&appid=581b9ce182221b37e54f248e484a108c")
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
     })
-}
+})
+    
