4119d11d2ba7ffe881cfe2d2be8cb013b3f0955e
catherinecunningham <cfunk512@gmail.com>
2022-08-22 18:40:18 -0700

    start bootstrap in html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var windInput = document.getElementById("wind-input")
 var humidityInput = document.getElementById("humidity-input")
 var UVInput = document.getElementById("UV-input")
 var currentCity = document.getElementById("current-city")
-var searchBtn = document.getElementById("search-Btn")
+var searchBtn = document.getElementById("search-btn")
 
 // Weather API
 searchBtn.addEventListener('click', function(e) {
@@ -13,7 +13,7 @@ searchBtn.addEventListener('click', function(e) {
     var longitude = "121.4944"
     var exclusions = "minutely, hourly, daily, alerts"
     
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=" + exclusions + "&appid=581b9ce182221b37e54f248e484a108c")
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude="+exclusions+"&appid=b265093aa7a118e11c4591d956102e2c")
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
