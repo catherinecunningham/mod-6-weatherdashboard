fac2df2fba36170fc330bb4b20aefbf344c2d556
catherinecunningham <cfunk512@gmail.com>
2022-08-23 20:04:16 -0700

    set up 5-day forecast API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var currentCity = document.getElementById("current-city")
 var searchBtn = document.getElementById("search-btn")
 var degree = "°F"
+var mph = " mph"
+var percent = "%"
 var day = document.getElementById("day-0")
 var temp = document.getElementById("temp-input")
 var wind = document.getElementById("wind-input")
@@ -58,8 +60,8 @@ searchBtn.addEventListener('click', function(e) {
         day.textContent = moment().format('l')
         currentCity.textContent = data.name 
         temp.textContent = "Temp: " + data.main.temp + degree
-        wind.textContent = "Wind Speed: " + data.wind.speed + " mph"
-        humidity.textContent = "Humidity: " + data.main.humidity + "%"
+        wind.textContent = "Wind Speed: " + data.wind.speed + mph
+        humidity.textContent = "Humidity: " + data.main.humidity + percent
 
        
 
@@ -72,7 +74,7 @@ searchBtn.addEventListener('click', function(e) {
         })
         //current date - convert from unix timestamp
 
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+ data.coord.lat +"&lon="+ data.coord.lon +"&exclude="+exclusions+"&appid=b265093aa7a118e11c4591d956102e2c")
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+ data.coord.lat +"&lon="+ data.coord.lon +"&exclude="+exclusions+"&appid=b265093aa7a118e11c4591d956102e2c&units=imperial")
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
@@ -80,6 +82,29 @@ searchBtn.addEventListener('click', function(e) {
         //5-days needed in array: 3, 11, 19, 27, 35
 
             day1.textContent = moment().add("days", 1).format('l')
+            temp1.textContent = "Temp: " + data.list[3].main.temp + degree
+            wind1.textContent = "Wind Speed: " + data.list[3].wind.speed + mph
+            humidity1.textContent = "Humidity: " + data.list[3].main.humidity + percent
+
+            day2.textContent = moment().add("days", 2).format('l')
+            temp2.textContent = "Temp: " + data.list[11].main.temp + degree
+            wind2.textContent = "Wind Speed: " + data.list[11].wind.speed + mph
+            humidity2.textContent = "Humidity: " + data.list[11].main.humidity + percent
+
+            day3.textContent = moment().add("days", 3).format('l')
+            temp3.textContent = "Temp: " + data.list[19].main.temp + degree
+            wind3.textContent = "Wind Speed: " + data.list[19].wind.speed + mph
+            humidity3.textContent = "Humidity: " + data.list[19].main.humidity + percent
+
+            day4.textContent = moment().add("days", 4).format('l')
+            temp4.textContent = "Temp: " + data.list[27].main.temp + degree
+            wind4.textContent = "Wind Speed: " + data.list[27].wind.speed + mph
+            humidity4.textContent = "Humidity: " + data.list[27].main.humidity + percent
+
+            day5.textContent = moment().add("days", 5).format('l')
+            temp5.textContent = "Temp: " + data.list[35].main.temp + degree
+            wind5.textContent = "Wind Speed: " + data.list[35].wind.speed + mph
+            humidity5.textContent = "Humidity: " + data.list[35].main.humidity + percent
 
         })
     })
